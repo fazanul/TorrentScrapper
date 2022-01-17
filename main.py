8939ca1490c6015025ff21f42e13da1aebfd79d9
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-01-17 19:29:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from pyrogram import Client, filters
 from pyrogram.errors.exceptions.bad_request_400 import MessageEmpty
 from config import API_ID, API_HASH, BOT_TOKEN
 
-options = webdriver.ChromeOptions
+options = webdriver.ChromeOptions()
 options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 options.add_argument("--headless")
 options.add_argument("--disable-dev-shm-usage")
