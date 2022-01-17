9b93ced6c24f4aa7d61430892ec4b44ef377786e
kevin nadar <jesikamaraj@gmail.com>
2022-01-17 12:32:35 +0530

    Initial commit

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+API_ID = os.environ.get("API ID")
+API_HASH = os.environ.get("API HASH")
+BOT_TOKEN = os.environ.get("BOT TOKEN")
---
+++ main.py
@@ -0,0 +1,59 @@
+# (c) @ask_admin001
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchWindowException, NoSuchElementException, ElementClickInterceptedException
+from pyrogram import Client, filters
+from selenium.webdriver.chrome.options import Options
+from  pyrogram.errors.exceptions.bad_request_400 import MessageEmpty
+from config import API_ID, API_HASH, BOT_TOKEN
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-infobars")
+driver = webdriver.Chrome(options=options)
+
+bot = Client(
+    "Web Scrapping Bot",
+    bot_token=BOT_TOKEN,
+    api_id=int(API_ID),
+    api_hash=API_HASH,
+)
+
+torrent = []
+
+
+@bot.on_message(filters.command('start'))
+async def start(bot, message):
+    await message.reply_text(message.chat.id, "Send Any 1tamilmv.com Link")
+
+
+@bot.on_message(filters.regex("index\.php\?/forums/topic"))
+async def link_regex(bot, message):
+    try:
+        txt = await message.reply_text("Scrapping torrent link, Please Wait")
+        link = str(message.text)
+        driver.get(link)
+        torrent_link = driver.find_elements(By.CLASS_NAME, "ipsAttachLink_block")
+        try:
+            title = driver.find_element(By.XPATH, '//h1').text
+        except NoSuchElementException:
+            title = ""
+        heading = f"{title}\n\n"
+        msg = ""
+        for link in torrent_link:
+            tor = link.get_attribute("href")
+            text = link.text
+            msg += f"Name : {text}\nTorrent: {tor}\n\n"
+        await message.reply_text(heading+msg)
+        await txt.delete()
+    except MessageEmpty:
+        await message.reply_text('Some error occurred')
+        await txt.delete()
+print("Bot running...")
+bot.run()
+
