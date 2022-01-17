39cd99071153ee0da1be2deb2773f12cfb22f72e
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-01-17 23:45:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,8 +49,9 @@ async def link_regex(bot, message):
         for link in torrent_link:
             tor = link.get_attribute("href")
             text = link.text
-            msg += f"Name : {text}\nTorrent: {tor}\n\n"
-        await message.reply_text(heading + msg)
+            msg = f"/leechfile {tor}"
+            await message.reply_text(msg)
+        
         await txt.delete()
     except MessageEmpty:
         await message.reply_text('Some error occurred')
