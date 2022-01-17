abf6273413a892c661ecf80b36ea84988ce2bf04
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-01-17 23:55:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ async def link_regex(bot, message):
         for link in torrent_link:
             tor = link.get_attribute("href")
             text = link.text
-            msg += f"Name : {text}\nTorrent: `{tor}`\n\n"
+            msg += f"Name : **{text}**\n\nTorrent: `{tor}`\n\n\n"
         await message.reply_text(heading + msg)
         await txt.delete()
     except MessageEmpty:
