3eee3dceb948828bcbd6b126025766432c9bd4fd
kevin nadar <jesikamaraj@gmail.com>
2022-01-18 12:19:15 +0530

    Update

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-API_ID = os.environ.get("API ID")
-API_HASH = os.environ.get("API HASH")
-BOT_TOKEN = os.environ.get("BOT TOKEN")
+API_ID = os.environ.get("API_ID")
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
