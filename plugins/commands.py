de72f892734f5f4f72e4cbb2d565cf23b3c8d3d3
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-01-21 00:00:53 +0530

    Delete plugins directory

--- plugins/commands.py
+++
@@ -1,6 +0,0 @@
-from pyrogram import filters, Client
-
-
-@Client.on_message(filters.command('start'))
-async def start(bot, message):
-    await message.reply_text("Send any 1tamilmv.com or tamilblasters.com link to scrap torrents")
