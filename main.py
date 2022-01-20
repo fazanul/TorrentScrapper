02dcd8b004f208d3cf96cc03edbe163d846e9166
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-01-20 21:02:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from selenium.common.exceptions import NoSuchElementException
 from pyrogram import Client, filters
 from pyrogram.errors.exceptions.bad_request_400 import MessageEmpty
 from config import API_ID, API_HASH, BOT_TOKEN
+from pyrogram.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
 
 options = webdriver.ChromeOptions()
 options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
@@ -29,9 +30,25 @@ bot = Client(
 torrent = []
 API_KEY = "3239u7cq042faca6z1m5"
 
+reply_markup = InlineKeyboardMarkup(
+    [
+        [  # First row
+            InlineKeyboardButton(  # Generates a callback query when pressed
+                "1TamilMV",
+                callback_data="1tamilmv"
+            ),
+            InlineKeyboardButton(  # Opens a web URL
+                "TamilBlasters",
+                callback_data="tamilblasters"
+            ),
+        ]
+    ]
+)
+
+
 @bot.on_message(filters.command('start'))
 async def start(bot, message):
-    await message.reply_text("Send Any 1tamilmv.com Link")
+    await message.reply_text("Send any 1tamilmv.com or tamilblasters.com link to scrap torrents")
 
 
 @bot.on_message(filters.regex("index\.php\?/forums/topic"))
@@ -85,33 +102,45 @@ async def get_shortlink(link):
         
 @bot.on_message(filters.command('list'))
 async def lists(c, m):
-    texts = ""
     querys = ""
     length = len(m.command)
     for query in m.command[1:length]:
         querys += f"{query} "
-    txt = await c.send_message(m.chat.id, f"Searching For: {querys}")
-    link = f"https://www.1tamilmv.golf/index.php?/search/&q={querys}&search_and_or=and&search_in=titles&sortby=relevancy"
-    driver.get(link)
-    title = driver.title
-    links = driver.find_elements(By.TAG_NAME, "h2")
-    msg = []
-    count = 0
-    try:
-        for link in links:
-            text = link.text
-            url = driver.find_element(By.LINK_TEXT, text).get_attribute("href")
-            count += 1
-            msgs = f"{count}. [{text}]({url})\n\n"
-            msg.append(msgs)
-    except NoSuchElementException:
-        pass
+    txt = await m.reply_text(m.chat.id, "Choose the website you want to search!", reply_markup=reply_markup)
 
-    for text in msg[0:20]:
-        texts += text
-    reply = f"**{title}**\n\n{texts}"
-    await c.send_message(m.chat.id, reply, disable_web_page_preview=True)
-    await txt.delete() 
+    @bot.on_callback_query()
+    async def cb_handler(client, query):
+        await query.message.edit_text(f"Searching For: {querys}")
+        texts = ""
+        command = ""
+        if query.data == "1tamilmv":
+            await query.answer()
+            command += "1tamilmv"
+        elif query.data == "tamilblasters":
+            await query.answer()
+            command += "tamilblasters"
+        link = f"https://www.{command}.com/index.php?/search/&q={querys}&search_and_or=and&search_in=titles&sortby=relevancy"
+        driver.get(link)
+        await asyncio.sleep(10)
+        title = driver.title
+        links = driver.find_elements(By.TAG_NAME, "h2")
+        msg = []
+        count = 0
+        try:
+            for link in links:
+                text = link.text
+                url = driver.find_element(By.LINK_TEXT, text).get_attribute("href")
+                count += 1
+                msgs = f"{count}. [{text}]({url})\n\n"
+                msg.append(msgs)
+        except NoSuchElementException:
+            pass
+
+        for text in msg[0:20]:
+            texts += text
+        reply = f"**{title}**\n\n{texts}"
+        await c.send_message(m.chat.id, reply, disable_web_page_preview=True)
+        await txt.delete() 
     
    
 
