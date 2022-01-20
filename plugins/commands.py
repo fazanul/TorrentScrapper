fa2cbd7f06dc6b90e2cad57ea6745d1578293ec4
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-01-20 23:34:16 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,6 +1,6 @@
-from pyrogram import filters
-from main import bot
+from pyrogram import filters, Client
 
-@bot.on_message(filters.command('start'))
+
+@Client.on_message(filters.command('start'))
 async def start(bot, message):
     await message.reply_text("Send any 1tamilmv.com or tamilblasters.com link to scrap torrents")
