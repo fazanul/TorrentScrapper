3a5600b50a0411c203786f488bf589c9ffea25d1
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-01-22 01:01:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -97,26 +97,6 @@ async def link_regex(bot, message):
         await txt.delete()
 
         
-@bot.on_message(filters.command('add_remote'))
-async def link_handler(bot, message):
-    try:
-        link = str(message.command[1])
-        short_link = await get_shortlink(link)
-        await message.reply(f"<code>https://videovard.sx/e/{short_link}</code>", quote=True)
-    except IndexError as e:
-        await message.reply(f'`/add_remote [link]`', quote=True)
-
-
-async def get_shortlink(link):
-    url = 'https://api.videovard.sx/v2/api/remote/add'
-    params = {'key': API_KEY, 'url': link}
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-            data = await response.json()
-            result = data["result"]
-            return result["filecode"]
-
 
 @bot.on_message(filters.command('listmv'))
 async def lists(c, m):
