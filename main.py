57598dc5f06f08f06a1b55fc2a16f5f3f0960d78
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-01-22 01:26:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # (c) @ask_admin001
 
 from pyrogram import Client
+from config import API_HASH, BOT_TOKEN, API_ID
 
 bot = Client(
     "Web Scrapping Bot",
+    plugins = dict(root="plugins"),
     bot_token=BOT_TOKEN,
     api_id=API_ID,
     api_hash=API_HASH,
