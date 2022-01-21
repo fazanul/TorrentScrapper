0662435dbe9f2aa33e4191671c8249f8708fad89
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-01-22 01:39:17 +0530

    Update messages.py

--- plugins/messages.py
+++ plugins/messages.py
@@ -1,15 +1,22 @@
 caption = """
 **👇 Click Here To Watch 👇
+
 200MB - Click
+
 400MB - Click
+
 700MB - Click
+
 1.40GB - Click
+
 Join - T.me/t2links**
 """
 
 msg = f"""
 **👇 Click Here To Watch 👇
+
 @
+
 Streaming Link - Will be uploaded**
 """
 
