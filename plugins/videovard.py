e65947d589e774df83631b0ab5b6990ffd112f12
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-01-23 01:24:16 +0530

    Update videovard.py

--- plugins/videovard.py
+++ plugins/videovard.py
@@ -33,41 +33,6 @@ async def files(bot, message):
             await message.reply_text(msg, quote=True)
 
 
-# Search Files in VideoVard
-@Client.on_message(filters.command('get_files'))
-async def get_files(bot, message):
-    async with aiohttp.ClientSession() as session:
-        try:
-            title = str(message.command[1])
-            msg = ""
-            url = 'https://api.videovard.sx/v2/api/file/list'
-            params = {'key': API_KEY,
-                      'page': 1,
-                      'per_page': 20,
-                      'title': title,
-                      }
-            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-
-                data = await response.json()
-                print(data)
-                result = data["result"]
-                file1 = result["files"]
-                count = 0
-                for file in file1:
-                    count += 1
-                    title = str(file["title"])
-                    link = file["link"]
-                    title1 = title.replace("'", "")
-                    print(title1)
-                    msg += f"{count}. <a href={link}>{title1}\n\n</a>"
-                if msg == "":
-                    await message.reply_text("No Movies Found", quote=True)
-                elif msg != "":
-                    await message.reply_text(msg, quote=True)
-        except IndexError:
-            await message.reply_text("<code>/get_files [query]</code>", quote=True)
-
-
 #remotely upload files to videovard.sx
 @Client.on_message(filters.command('add_remote'))
 async def link_handler(bot, message):
