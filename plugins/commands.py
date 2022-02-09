f9ec2037a0519f23ebdeb29177badf1181f8117f
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-02-10 00:40:26 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,6 +1,21 @@
 from pyrogram import Client, filters
 
+txt = '''
+Commands:
+
+/listmv [query] - to search movies in TamilMV
+
+/listbl [query] - to search movies in Tamilblasters
+
+/post [tamilmv link] - makes a post of a given link
+
+/latest - to get screenshot of latest movies in TamilMV and Tamilblasters
+
+Bot Usage:
+
+Send any 1tamilmv.com or tamilblasters.com link to scrap torrents
+'''
 
 @Client.on_message(filters.command('start'))
 async def start(bot, message):
-    await message.reply_text("Send any 1tamilmv.com or tamilblasters.com link to scrap torrents")
+    await message.reply_text(txt)
