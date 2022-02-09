b27844f4bd60039fe7296ddfaa86c144d869bc31
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-02-10 00:45:43 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -9,28 +9,29 @@ from config import API_KEY
 #Rename Files in VideoVard
 @Client.on_message(filters.command('rename_file'))
 async def get_files(bot, message):
-    async with aiohttp.ClientSession() as session:
-        try:
-            print(len(message.command))
-            lens = len(message.command) + 1
-            print(lens)
-            titles = ""
-            for i in message.command[2:lens]:
-                titles += i + " "
+    if API_KEY:
+        async with aiohttp.ClientSession() as session:
+            try:
+                print(len(message.command))
+                lens = len(message.command) + 1
+                print(lens)
+                titles = ""
+                for i in message.command[2:lens]:
+                    titles += i + " "
+                    print(titles)
                 print(titles)
-            print(titles)
-            file_code = str(message.command[1])
-            url = 'https://api.videovard.sx/v2/api/file/rename'
-            params = {'key': API_KEY,
-                      'title': titles,
-                      'file_code': file_code
-                      }
-            info = await file_info(file_code)
-            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-                data = await response.json()
-            await message.reply_text(f"{info} renamed to [{titles}](https://videovard.sx/e/{file_code})")
-        except Exception:
-            await message.reply_text("<code>/rename_file [file_code] [title]</code>", quote=True, disable_web_page_preview=True)
+                file_code = str(message.command[1])
+                url = 'https://api.videovard.sx/v2/api/file/rename'
+                params = {'key': API_KEY,
+                          'title': titles,
+                          'file_code': file_code
+                          }
+                info = await file_info(file_code)
+                async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                    data = await response.json()
+                await message.reply_text(f"{info} renamed to [{titles}](https://videovard.sx/e/{file_code})")
+            except Exception:
+                await message.reply_text("<code>/rename_file [file_code] [title]</code>", quote=True, disable_web_page_preview=True)
 
 
 async def file_info(file_code):
